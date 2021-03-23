6e9618e15a99a5e6e2a9ad8750f92573fb97fbe1
Mahdi <mahdirefaideen@gmail.com>
2021-03-23 22:36:00 +0530

    generate SQL

--- src/find-form/FindForm.js
+++ src/find-form/FindForm.js
@@ -5,20 +5,42 @@ import axios from 'axios';
 class FindForm extends Component {
     constructor() {
         super();
-        this.state = { value: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { query: '', sql_result: '' };
+        this.handleQueryChange = this.handleQueryChange.bind(this);
+        this.handleSqlResultChange = this.handleSqlResultChange.bind(this);
+        this.generateSql = this.generateSql.bind(this);
+        this.executeSql = this.executeSql.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
+    handleQueryChange(event) {
+        this.setState({ query: event.target.value });
     }
 
-    handleSubmit(event) {
+    handleSqlResultChange(event) {
+        this.setState({ sql_result: event.target.value });
+    }
+
+    generateSql(event) {
         event.preventDefault();
-        console.log(this.state.value)
-        let jsonObj = { query: this.state.value};
+        console.log(this.state.query)
+        let jsonObj = { query: this.state.query};
         axios.post('http://localhost:5000/query', jsonObj)
+        .then(response => {
+            // this.setState({ articleId: response.data.id })
+            console.log(response);
+            this.setState({ sql_result: response.data})
+        })
+        .catch(error => {
+            // this.setState({ errorMessage: error.message });
+            console.error('There was an error!', error);
+        });
+    }
+
+    executeSql(event) {
+        event.preventDefault();
+        console.log(this.state.sql_result)
+        let jsonObj = { sql: this.state.sql_result};
+        axios.post('http://localhost:5000/execute', jsonObj)
         .then(response => {
             // this.setState({ articleId: response.data.id })
             console.log(response);
@@ -32,11 +54,15 @@ class FindForm extends Component {
     render() {
         return (
             <div className="formDiv">
-                <form onSubmit={this.handleSubmit}>
-                    <label className="label">What do you want?</label>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-
-                    <input type="submit" value="Submit" />
+                {/* <form onSubmit={this.handleSubmit}> */}
+                <form>
+                    <label className="label">කුමක්ද ඔබ සොයන්නේ?</label>
+                    <input type="text" value={this.state.query} onChange={this.handleQueryChange} 
+                        placeholder='උ.දා: ලකුනු 75ට වැඩි සිසුන්ගේ විස්තර ලබාදෙන්න'/>
+                    <input type="submit" value="SQL ලෙස පරිවර්තනය කරන්න" onClick={this.generateSql} />
+                    <br></br>
+                    <input type="text" value={this.state.sql_result} onChange={this.handleSqlResultChange} />
+                    <input type="submit" value="දත්ත ලබාගන්න" onClick={this.executeSql} />
                 </form>
             </div>
 
